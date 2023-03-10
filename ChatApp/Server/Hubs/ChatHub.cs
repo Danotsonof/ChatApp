8f1673a7554b69ffb50c2b88de7ef1cf3c124303
Daniel Otulagun <Danotsonof@gmail.com>
2023-03-10 08:54:48 +0100

    Add project files.

---
+++ ChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            if (Users.ContainsValue(username))
+                await SendAlert(username);
+            else
+            {
+                Users.Add(Context.ConnectionId, username);
+                await AddMessageToChat("", $"{username} joined chat");
+                await base.OnConnectedAsync();
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(x => x.Key == Context.ConnectionId).Value;
+            await AddMessageToChat("", $"{username} left chat.");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("GeneralRoom", user, message);
+        }
+
+        public async Task SendAlert(string user)
+        {
+            await Clients.All.SendAsync("Alert", user);
+        }
+    }
+}
